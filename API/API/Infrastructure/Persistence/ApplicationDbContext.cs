29069f85bdcc5ea9dfaa92048a06403f920af7d4
Moustapha Dieye <moustaphadieye96@gmail.com>
2023-10-20 11:52:36 +0000

    connection à la base données

---
+++ API/API/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
