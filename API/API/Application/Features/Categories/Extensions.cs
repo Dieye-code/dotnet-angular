fc69b1736b47de1f5bbcc88b653f23e02cb8058a
Moustapha Dieye <moustaphadieye96@gmail.com>
2023-10-23 17:43:34 +0000

    get category query

---
+++ API/API/Application/Features/Categories/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using API.Domain.Entities;
+using API.Models;
+
+namespace API.Application.Features.Categories;
+
+public static class Extensions
+{
+    public static CategoryDto ProjectToCategoryDto(this Category category)
+    {
+        return new CategoryDto(category.Id, category.Libelle, category.CreatedAt, category.UpdatedAt, category.DeletedAt);
+    }
+}
---
+++ API/API/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using API.Application.Features.Categories.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("categories")]
+[ApiController]
+public class CategoryController : ApiControllerBase
+{
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var categories = await Mediator.Send(new GetCategoryQuery());
+        return Ok(categories);
+    }
+}
